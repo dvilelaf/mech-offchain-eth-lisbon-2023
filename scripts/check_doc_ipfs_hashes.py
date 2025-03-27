9152006dc5ce372256ade8c40832de734745b8d6
David Vilela <dvilelaf@gmail.com>
2025-03-27 13:35:34 +0100

    fix: ipfs script

--- scripts/check_doc_ipfs_hashes.py
+++ scripts/check_doc_ipfs_hashes.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
 #
-#   Copyright 2022-2023 Valory AG
+#   Copyright 2022-2024 Valory AG
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -82,6 +82,10 @@ class Package:  # pylint: disable=too-few-public-methods
         self.hash = package_hash
 
         if self.name == "scaffold":
+            return
+
+        if self.type == "customs":
+            print(f"Package: {self.package_id} is a customs package. Skipping...")
             return
 
         if self.type not in (
@@ -168,7 +172,7 @@ class PackageHashManager:
             # No match
             if not m_command and not m_package:
                 print(
-                    f"[{target_file}]: line {package_line!r} does not match an autonomy/aea command or package format"
+                    f"[{target_file}]: line '{package_line!r}' does not match an autonomy/aea command or package format"
                 )
                 return None
             m = m_command or m_package
@@ -185,8 +189,7 @@ class PackageHashManager:
 
                 # This hash does not exist in packages.json
                 print(
-                    f"[{target_file}]: unknown IPFS hash in line {package_line!r}. "
-                    "Can't fix because this command just uses the hash"
+                    f"[{target_file}]: unknown IPFS hash in line '{package_line!r}'. Can't fix because this command just uses the hash"
                 )
                 return None
 
@@ -221,8 +224,7 @@ class PackageHashManager:
 
                 if not package_type:
                     raise ValueError(
-                        f"[{target_file}]: could not infer the package type for line {package_line!r}"
-                        "\nPlease update the hash manually."
+                        f"[{target_file}]: could not infer the package type for line '{package_line!r}'\nPlease update the hash manually."
                     )
 
             return self.package_tree[d["vendor"]][package_type][d["package"]].hash
@@ -230,7 +232,7 @@ class PackageHashManager:
         # Otherwise log the error
         except KeyError:
             print(
-                f"[{target_file}]: could not find the corresponding hash for line {package_line!r}"
+                f"[{target_file}]: could not find the corresponding hash for line '{package_line!r}'"
             )
             return None
 
@@ -354,9 +356,7 @@ def check_ipfs_hashes(  # pylint: disable=too-many-locals,too-many-statements
         sys.exit(1)
 
     if matches == 0:
-        print(
-            "No commands were found in the docs. The command regex might be outdated."
-        )
+        print("No commands were found in the docs.")
 
     print("Checking doc IPFS hashes finished successfully.")
 
